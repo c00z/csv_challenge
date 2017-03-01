a7cd0ddfd337158629709a48e0951328e8e7bc26
c00z <zcusimano@gmail.com>
2017-03-01 10:16:58 -0800

    further a long in the solution

--- work.rb
+++ work.rb
@@ -26,18 +26,56 @@ end
 
 
 
-# comma input
-File.foreach('./sample/comma.txt') do |line|
+@attendance = []
 
+# comma input
+File.foreach('./sample/comma.txt') do |postition|
+  postition = postition.split(', ')
+  @attendance.push(
+    Student.new({
+      first: postition[1],
+      last: postition[0],
+      gender: postition[2],
+      color: postition[3],
+      dob: postition[4]
+    })
+  )
 end
 
 # pipe input
-File.foreach('./sample/pipe.txt') do |line|
-
+File.foreach('./sample/pipe.txt') do |postition|
+  postition = postition.split('| ')
+  @attendance.push(
+    Student.new({
+      first: postition[1],
+      last: postition[0],
+      gender: postition[3],
+      color: postition[4],
+      dob: postition[5]
+    })
+  )
 end
 
 
 # space input
-File.foreach('./sample/space.txt') do |line|
+File.foreach('./sample/space.txt') do |postition|
+  postition = postition.gsub(/\s+/m, ' ').strip.split(" ")
+  @attendance.push(
+    Student.new({
+      first: postition[1],
+      last: postition[0],
+      gender: postition[3],
+      color: postition[5],
+      dob: postition[4]
+    })
+  )
+end
 
+def output_1 ()
+end
+
+def output_2 ()
+end
+
+def output_3 ()
 end
